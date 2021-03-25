386b8d90af4de133243f4372592d284faa7837de
alexnimjli <alexnimjli@gmail.com>
2021-03-25 14:35:34 +0000

    fixing method calling bugs

--- predict.py
+++ predict.py
@@ -18,8 +18,11 @@ def predict_rating(review, model, vectorizer, decision_threshold=0.5):
     """
     review = simple_normalize(review)
     # can use more complicated normalize_corpus fucntion here from NormalizeText.py
-
+    print(review)
     vectorized_review = torch.tensor(vectorizer.vectorize(review))
+
+    print(vectorized_review.shape)
+
     result = model(vectorized_review.view(1, -1))
 
     probability_value = torch.sigmoid(result).item()
@@ -27,7 +30,7 @@ def predict_rating(review, model, vectorizer, decision_threshold=0.5):
     if probability_value < decision_threshold:
         index = 0
 
-    return probability_value, vectorizer.rating_vocab.lookup_index(index)
+    return probability_value, vectorizer.y_values_vocab.lookup_index(index)
 
 
 if __name__ == "__main__":
@@ -35,14 +38,16 @@ if __name__ == "__main__":
         cuda = False
     device = torch.device("cuda" if cuda else "cpu")
 
+    # should add functionality that allows to load a saved vectorizer instead
     dataset = TextDataset(config.data_path)
     vectorizer = dataset.get_vectorizer()
 
-    model = SingleLayerPerceptron(num_features=len(vectorizer.review_vocab))
+    model = SingleLayerPerceptron(num_features=len(vectorizer.text_vocab))
     model.load_state_dict(torch.load(config.save_dir+config.model_filename))
     model = model.to(device)
 
-    review = 'I think it was pretty good'
+    review = 'terrible film, never going to watch it again'
+    print(review)
 
     probability_value, sentiment = predict_rating(review, model, vectorizer, decision_threshold=0.5)
     print(probability_value)
